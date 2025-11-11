b53c1964f624f7c0c3da7ec508271f80804495e4
Kat <katherinekelli.creates@gmail.com>
2025-11-11 07:58:39 -0600

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Katherine Pizzirusso on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
