99e396ae3acb7154826d253c1903352e190b735c
Kat <katherinekelli.creates@gmail.com>
2025-11-11 11:46:48 -0600

    Added rounded blue tiles

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -13,7 +13,12 @@ struct ContentView: View {
             Text("Tic Tac Toe").font(.title).bold()
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3)) {
                 ForEach(0..<9) { index in
-                    Color.blue
+                    ZStack {
+                        Color.blue
+                        Text("X")
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
                         .frame(width: 120, height: 120, alignment: .center)
                         .cornerRadius(30)
                 }
